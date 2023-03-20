f7dc2c26d42b4f9b3c08392db2e44c87b546b403
haomingjue <haomingjue@outlook.com>
2023-03-19 23:33:29 -0400

    feat: updated API for new algorithms

--- ml/views.py
+++ ml/views.py
@@ -6,6 +6,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 import joblib
 import os
+import pandas as pd
 
 base_dir = os.path.dirname(__file__)
 
@@ -14,8 +15,22 @@ base_dir = os.path.dirname(__file__)
 @csrf_exempt
 @api_view(['POST'])
 def predict(request, model_name):
-    data = [request.data["input"]]
-
+    data = request.data["input"]
+    input_data = pd.DataFrame({
+        'age': data[0],
+        'sex': data[1],
+        'cp': data[2],
+        'trestbps': data[3],
+        'chol': data[4],
+        'fbs': data[5],
+        'restecg': data[6],
+        'thalach': data[7],
+        'exang': data[8],
+        'oldpeak': data[9],
+        'slope': data[10],
+        'ca': data[11],
+        'thal': data[12]
+    }, index=range(1)) if model_name == "XGB" else [data]
     load_model = joblib.load(open(base_dir + "/saved_models/" + model_name + ".joblib", 'rb'))
-    y_pred = load_model.predict(data)
-    return JsonResponse(y_pred[0], safe=False)
+    y_pred = load_model.predict(input_data)
+    return JsonResponse(int(y_pred[0]), safe=False)
